264c3ba3603594b27d979b2b8e8eaf22e47a7820
leeandri <rl.tsirih@gmail.com>
2021-12-08 17:52:04 +0300

    15.param converter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -69,18 +69,12 @@ EOF
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
 
-        $repository = $entityManager->getRepository(Question::class);
-        /** @var Question|null $question */
-        $question = $repository->findOneBy(['slug' => $slug]);
-        if (!$question){
-            throw $this->createNotFoundException(sprintf('no question found for slug "%s', $slug));
-        }
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
