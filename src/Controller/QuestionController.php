1d04345d9f6246f301f6d82b3d090709e301906d
leeandri <rl.tsirih@gmail.com>
2021-12-08 20:56:23 +0300

    18.update

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -94,16 +94,20 @@ EOF
     /**
      * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
      */
-    public function questionVote(Question $question, Request $request)
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
     {
         $direction = $request->request->get('direction');
 
         if ($direction === 'up') {
-            $question->setVotes($question->getVotes() + 1);
+            $question->upVote();
         } elseif ($direction === 'down') {
-            $question->setVotes($question->getVotes() - 1);
+            $question->downVote();
         }
 
-        dd($question);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_question_show',[
+            'slug' => $question->getSlug()
+        ]);
     }
 }
